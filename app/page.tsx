9fc5137e546396e790cc3dce22dfee0c1daa5070
Khuslee1 <Khuselee11@gmail.com>
2026-02-24 12:13:56 +0800

    PrismaYes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { Generate } from "./components/Generate";
 import { Quiz } from "./components/Quiz";
 import { ResultQuiz } from "./components/ResultQuiz";
 import { SummarizedArticle } from "./components/SummarizedArticle";
-import { SummarizedContent } from "./components/SummarizedContent";
 
 export default function Home() {
   return (
